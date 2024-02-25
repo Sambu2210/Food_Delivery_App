abea8ccf073f987d434dc703a401ac7967ba2d4e
balaji <128803455+Sambu2210@users.noreply.github.com>
2024-02-25 15:39:04 +0530

    authendication user links

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -1,9 +1,12 @@
-import { Menu } from "lucide-react"
+import { CircleUserRoundIcon, Menu } from "lucide-react"
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet"
 import { Separator } from "@radix-ui/react-separator"
 import { Button } from "./ui/button"
+import { useAuth0 } from "@auth0/auth0-react"
+import MobileNavLinks from "./MobileNavLinks"
 
 const MobileNav = () => {
+    const {isAuthenticated, loginWithRedirect, user} = useAuth0();
   return (
     <Sheet>
         <SheetTrigger>
@@ -11,11 +14,22 @@ const MobileNav = () => {
         </SheetTrigger>
         <SheetContent className="space-y-3">
             <SheetTitle>
-                <span>Welcome to MernEat.com!</span>
+                {isAuthenticated ? (
+                <span className="flex items-center font-bold gap-2">
+                    <CircleUserRoundIcon className="text-orange-500"/>
+                    {user?.email}
+                </span>
+                ) :(
+                    <span>Welcome to MernEat.com!</span>
+                )}
             </SheetTitle>
             <Separator/>
-            <SheetDescription className="flex">
-                <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+            <SheetDescription className="flex flex-col gap-4">
+                {isAuthenticated ? (
+                <MobileNavLinks/>
+                ):(
+                    <Button className="flex-1 font-bold bg-orange-500" onClick={() => loginWithRedirect()}>Log In</Button>
+                )}
             </SheetDescription>
         </SheetContent>
     </Sheet>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './global.css'
 import {BrowserRouter as Router} from "react-router-dom";
 import AppRoute from './AppRoute';
+import Auth0ProviderNavigate from './auth/Auth0ProviderNavigate';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      <AppRoute/>
+      <Auth0ProviderNavigate>
+        <AppRoute/>
+      </Auth0ProviderNavigate>
     </Router>
   </React.StrictMode>,
 )
